3c815347b87e4264c3e2cf3c300805bf4a800504
Habib5634 <hero5276311@gmail.com>
2024-12-30 22:34:51 +0500

    done

--- src/components/FeaturedCars/index.jsx
+++ src/components/FeaturedCars/index.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const FeaturedCars = () => {
   return (
-    <div className='h-screen'>
+    <div id='featured' className='bg-white  px-6 md:px-10 py-32 xl:py-40 w-full relative overflow-hidden h-screen'>
+    <div className=' max-w-[1440px] mx-auto w-full gap-5 relative z-[0.1]'>
+      <h1 className='text-[35px] md:text-[40px] text-center lg:text-[48px] xl:text-[60px] text-white font-medium mb-8 leading-tight'>Featured Section</h1>
+
       Featured Cars
+    </div>
     </div>
   )
 }
